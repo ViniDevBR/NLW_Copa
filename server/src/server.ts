0db1ff809c12a0b46e0de9f67db2181cf4e33aab
ViniDevBR <vinigtr386@gmail.com>
2022-11-01 13:37:12 -0300

    :tada: Finish Server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,32 @@
+//FASTIFY
 import Fastify from "fastify";
+import fastifyCors from "@fastify/cors";
+
+//PRISMA
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({
+  log: ['query']
+})
 
 async function bootstrap() {
   const fastify = Fastify({
     logger: true
   })
 
+  await fastify.register(fastifyCors, {
+    origin: true
+  })
   //localhost:4444/pools/count
-  fastify.get('/pools/count',() => {
+  fastify.get('/pools/count', async() => {
+    const countPools = await prisma.pool.count()
+
     return {
-      count: 123456
+      countPools
     }
   })
 
-  await fastify.listen({ port: 4444 })
+  await fastify.listen({ port: 4444, host: '0.0.0.0' })
 }
 
 bootstrap()
