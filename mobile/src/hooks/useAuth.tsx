823ad41029b4c83427119bbb1cefb58fbba22fad
ViniDevBR <vinigtr386@gmail.com>
2022-11-04 12:36:59 -0300

    :fire: New custom Hook

---
+++ mobile/src/hooks/useAuth.tsx
@@ -0,0 +1,8 @@
+import { AuthContext, AuthContextDataProps } from '../contexts/AuthContext'
+import { useContext }from 'react'
+
+export function useAuth(): AuthContextDataProps {
+  const context = useContext(AuthContext)
+  
+  return context
+}
